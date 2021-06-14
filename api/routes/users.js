b6c155a01e42b09acf659b16802b16aea868a29a
TeaDrinker <81148886+TeaDrinker12@users.noreply.github.com>
2021-06-14 11:10:37 +0300

    implement login and sign up

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,38 +1,56 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 
-router.post('/s/signup', (req, res) => {
+router.post('/s', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    console.log(email);
-    console.log(password);
-    // res.status(200);
-    // return
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        email: email,
-        password: password,
-    });
 
-    user
-    .save()
-    .then(result => {
-        return res.status(200).json({
-            message: 'Created',
-            user: {
-                email: email,
-                password: password,
-                result: result
+    User.find({email: email})
+        .exec()
+        .then(users => {
+            if (users.length >= 1) {
+                return res.status(409).json({
+                    message: 'Email already exists'
+                });
+            } else {
+                if (password === "") {
+                    return res.status(500).json({
+                        message: "Password cannot be empty"
+                    });
+                }
+                bcrypt.hash(password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json(err);
+                    }
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        email: email,
+                        password: hash,
+                    });
+                    user
+                    .save()
+                    .then(result => {
+                        res.status(200).json({
+                            message: 'User created',
+                            user: {
+                                email: email,
+                                password: password,
+                            }
+                        });
+                    })
+                    .catch(error => {
+                        res.status(500).json(error);
+                    });
+                });
             }
+        })
+        .catch(err => {
+            return res.json(err);
         });
-    })
-    .catch( error => {
-        res.status(500).json(error);
-    });
-
 });
 
 router.post('/', (req, res, next) => {
@@ -41,18 +59,29 @@ router.post('/', (req, res, next) => {
     User.find({email: email})
     .exec()
     .then( users => {
-        const user = users[0]
-        if (password === req.body.password)
-        return res.status(201).json({
-            message: 'User login',
-            user: {
-                username: user.email,
-                password: user.password,
+        const user = users[0];
+        bcrypt.compare(password, user.password, (err, result) => {
+            if (err) {
+                return res.status(500).json(err);
             }
+            if (result == false) {
+                return res.status(500).json({
+                    message: 'Invalid login'
+                });
+            }
+            return res.status(201).json({
+                message: 'User login',
+                user: {
+                    username: user.email
+                }
+            });
         });
+        
     })
     .catch( result => {
-        return res.status(500).json(result);
+        return res.status(500).json({
+            message: 'Invalid login'
+        });
     });
 });
 

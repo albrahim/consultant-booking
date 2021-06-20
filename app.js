26d362b2164f8503d8f111b5b051af29cb857b12
TeaDrinker <81148886+TeaDrinker12@users.noreply.github.com>
2021-06-20 10:48:59 +0300

    change database string

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://127.0.0.1:27017/booking');
-mongoose.connect('mongodb://auser:auser123456789@cluster0-shard-00-00.lrtps.mongodb.net:27017,cluster0-shard-00-01.lrtps.mongodb.net:27017,cluster0-shard-00-02.lrtps.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-yhlgm9-shard-0&authSource=admin&retryWrites=true&w=majority');
+mongoose.connect('mongodb://auser:auser123456789@cluster0-shard-00-00.lrtps.mongodb.net:27017,cluster0-shard-00-01.lrtps.mongodb.net:27017,cluster0-shard-00-02.lrtps.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-yhlgm9-shard-0&authSource=admin&retryWrites=true&w=majority?authSource=myFirstDatabase&w=1');
 
 const app = express();
 app.use(morgan('dev'));

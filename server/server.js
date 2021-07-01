66c640bcc346cf69d016d520dab9df3bea9e07f6
= <81148886+TeaDrinker12@users.noreply.github.com>
2021-07-01 11:00:53 +0300

    Add nodemailer code

--- server/server.js
+++ server/server.js
@@ -4,6 +4,36 @@ const app = require('./app');
 const port = process.env.PORT || 3003;
 console.log('Port: ' + port);
 
+/*
+const nodemailer = require('nodemailer');
+async function main() {
+    console.log("will create transport");
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        auth: {
+            user: 'lloyd75@ethereal.email',
+            pass: 'BbcPe8t2wx3hqZanTW',
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+    console.log('will send mail')
+    let info = await transporter.sendMail({
+        from: '"Lloyd Stanton" <lloyd75@ethereal.email>',
+        to: "bar@example.com, baz@example.com",
+        subject: "Hello",
+        text: "Hello World?",
+        html: "<b>Hello World?</b>"
+    });
+
+    console.log(`Message sent: ${info.messageId}`);
+    console.log(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
+}
+main().catch(console.error);
+*/
+
 const server = http.createServer(app);
 
 server.listen(port, () => {

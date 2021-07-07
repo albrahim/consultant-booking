cc867e2775cbdb136962064d8bff009f28299329
= <81148886+TeaDrinker12@users.noreply.github.com>
2021-07-07 13:02:37 +0300

    Remove mongoose import

--- server/api/routes/bookings.js
+++ server/api/routes/bookings.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 
-const User = require('../models/user');
 const Profile = require('../models/profile');
 const Booking = require('../models/booking');
 
--- server/api/routes/user-ids.js
+++ server/api/routes/user-ids.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const User = require('../models/user');

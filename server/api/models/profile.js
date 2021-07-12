45f29d60955bb60c059076bad5ebaec3af4b4383
= <81148886+TeaDrinker12@users.noreply.github.com>
2021-07-12 12:15:18 +0300

    Move profile into user document

--- server/api/models/profile.js
+++ server/api/models/profile.js
@@ -2,12 +2,6 @@ const mongoose = require('mongoose');
 const sessionTimeSchema = require('./session-time').schema;
 
 const profileSchema = mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-        unique: true
-    },
     firstName: String,
     lastName: String,
     gender: {
